ff36b1f036769302cff6e99704837f5fd9994063
corinnecm <corinne.carlson5@gmail.com>
2017-05-19 14:09:29 -0700

    re-organized model testing a little

--- answer_model_tester.py
+++ answer_model_tester.py
@@ -92,13 +92,26 @@ class FindOptimalModels(object):
         optimal_params = []
         params = params_dict.keys()
         for idx, model in enumerate(model_list):
-            gs = GridSearchCV(model, params_dict[params[idx]], n_jobs=1, cv=3)
+            gs = GridSearchCV(model, params_dict[params[idx]], n_jobs=-1, cv=3)
             gs.fit(self.X_train, self.y_train)
             output_msg.append('Best parameters for {} are {}'.format(params[idx],
                               gs.best_params_))
             optimal_params.append(gs.best_params_)
         print output_msg
         return optimal_params
+
+    def baseline_model(self):
+        '''
+        Predicts the score for a post, always predicts the average of the
+        normalized scores.
+
+        RETURNS:
+            R-squared score and MSE for the baseline predictions.
+        '''
+        avg_normed_score = y_train.mean()
+        y_pred = [avg_normed_score for row in xrange(len(self.y_test))]
+        print('Baseline R-squared: ', r2_score(self.y_test, y_pred))
+        print('Baseline MSE: ', mean_squared_error(self.y_test, y_pred))
 
 
 def run_optimized_models(model_list, optimal_params, X_test, X_train, y_test, y_train):
@@ -114,42 +127,3 @@ def run_optimized_models(model_list, optimal_params, X_test, X_train, y_test, y_
     for idx, mod in enumerate(model_list):
         model = mod(opt_params[idx])
     pass
-
-
-def baseline_model(y_train, y_test):
-    '''
-    Predicts the score for a post, always predicts the average of the
-    normalized scores.
-
-    RETURNS:
-        R-squared score and MSE for the baseline predictions.
-    '''
-    avg_normed_score = y_train.mean()
-    y_pred = [avg_normed_score for row in xrange(len(y_test))]
-    print('Baseline R-squared: ', r2_score(y_test, y_pred))
-    print('Baseline MSE: ', mean_squared_error(y_test, y_pred))
-
-
-if __name__ == '__main__':
-    # q = create_questions_df(100000)
-    # q_train_dc = DataCleaner(q)
-    # X, y = q_train_dc.get_clean()
-
-    a = create_answers_df(100000)
-    a_train_dc = DataCleaner(a)
-    A, b = a_train_dc.get_clean()
-
-
-    default_models = [RandomForestRegressor, GradientBoostingRegressor]
-
-    param_dict = {'rf': {'n_estimators': [10, 20, 50, 100, 5000], 'max_depth':
-                  [2, 3, 5], 'criterion': ['mse', 'mae']},
-                  'gbr': {'learning_rate': [.001, .01, .1, .2], 'max_depth':
-                          [2, 3, 5], 'n_estimators': [10, 20, 50, 100, 5000]}}
-
-    finder = FindOptimalModels(X_test, X_train, y_test, y_train, question=True,
-                               time_split=True)
-    fitted_models = finder.run_default_models(default_models)
-    opt_params = finder.run_grid_search(fitted_models, param_dict)
-
-    baseline_model(y_train, y_test)
---
+++ questions_score_random.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import mean_squared_error, r2_score, f1_score
+
+from question_query import create_questions_df
+from answer_query import create_answers_df
+from data_cleaning import DataCleaner
+from model_tester import FindOptimalModels
+
+
+if __name__ == '__main__':
+    q = create_questions_df(1000000)
+    q_train_dc = DataCleaner(q)
+    X, y = q_train_dc.get_clean()
+
+    default_models = [RandomForestRegressor, GradientBoostingRegressor]
+
+    param_dict = {'rf': {'n_estimators': [50, 100, 5000], 'max_depth':
+                  [2, 3, 5]},
+                  'gbr': {'learning_rate': [.001, .01, .1, .2], 'max_depth':
+                          [2, 3, 5], 'n_estimators': [50, 100, 5000]}}
+
+    finder = FindOptimalModels(X, y, question=True, time_split=False)
+    finder.baseline_model()
+    fitted_models = finder.run_default_models(default_models)
+    opt_params = finder.run_grid_search(fitted_models, param_dict)
