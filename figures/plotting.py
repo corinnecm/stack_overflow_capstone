0448bcdbfb2e7b1f0cca3b8fb903ca878839e76a
corinnecm <corinne.carlson5@gmail.com>
2017-05-22 17:09:14 -0700

    updated plotting script

--- figures/plotting.py
+++ figures/plotting.py
@@ -1,14 +1,24 @@
 import matplotlib.pyplot as pyplot
 import pandas as pd
 import numpy as np
-from pandas.tools.plotting import scatter_plot
+from pandas.plotting import scatter_plot
 
 plt.switch_backend('png')
+
+frames = [finder.X_train, finder.y_train]
+
+df = pd.concat(frames, axis=1)
+
 sm = scatter_matrix(df.sample(1000), alpha=0.3, figsize=(10, 10), diagonal='kde')
+
 [s.xaxis.label.set_rotation(45) for s in sm.reshape(-1)]
 [s.yaxis.label.set_rotation(0) for s in sm.reshape(-1)]
+
 [s.get_yaxis().set_label_coords(-0.3, 0.5) for s in sm.reshape(-1)]
+
 [s.set_xticks(()) for s in sm.reshape(-1)]
 [s.set_yticks(()) for s in sm.reshape(-1)]
+
 fig = sm[0, 0].get_figure()
+
 fig.savefig('file_name.png')
